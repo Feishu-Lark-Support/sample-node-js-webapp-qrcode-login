027dee8dd22392567105aeadcd1aec22d8ff51c1
王青阳 <wangqingyang.0403@bytedance.com>
2021-12-03 14:42:57 +0800

    fix Desensitization

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ export const PORT = 9000;
 export const PASSPORT_OAUTH_TOKEN = 'https://passport.feishu.cn/suite/passport/oauth/token';
 export const PASSPORT_OAUTH_USERINFO = 'https://passport.feishu.cn/suite/passport/oauth/userinfo';
 
-export const REDIRECT_URI = 'http://127.0.0.1:9000/login';
+export const REDIRECT_URI = 'Enter_the_Redirect_URI_Here'; // 参考文档：https://open.feishu.cn/document/uYjL24iN/uYjN3QjL2YzN04iN2cDN
 
-export const CLIENT_ID = 'cli_a03ee1eb59b9500e';
-export const CLIENT_SECRET = 'W50q9f8mhOYxx7o1clYuEdIBEiTYXc0c';
+export const CLIENT_ID = 'Enter_the_Application_Id_Here';
+export const CLIENT_SECRET = 'Enter_the_Application_Secret_Here';
