5e3f61b0027c654971699ea250882b2c0093c7f8
王青阳 <wangqingyang.0403@bytedance.com>
2021-12-10 17:02:35 +0800

    change pure html to artTemplate

--- src/handler/home.ts
+++ src/handler/home.ts
@@ -1,7 +1,15 @@
+import path from "path";
+import * as fs from "fs";
+import * as template from 'art-template';
+
 import {Handler} from "../route";
 import {Request, Response} from "express";
-import path from "path";
 
-export const indexHandler: Handler = async (req: Request, res:Response) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'))
+export const indexHandler: Handler = async (req: Request, res: Response) => {
+    const fileObj = fs.readFileSync(path.resolve(__dirname, '../views/index.html'));
+    const htmlStr = template.render(fileObj.toString(), {
+        CLIENT_ID: process.env.CLIENT_ID,
+        REDIRECT_URI: process.env.REDIRECT_URI,
+    });
+    res.send(htmlStr);
 };
